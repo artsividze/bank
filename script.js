423c051e824ee503762ca1d8450fd8fd42df0b64
artsividze <128794947+artsividze@users.noreply.github.com>
2024-03-08 12:53:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -63,47 +63,40 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
+
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const html = `
       <div class="movements__row">
-          <div class="movements__type movements__type--${type}">
-              ${i + 1} ${type}
-          </div>
-          <div class="movements__value">${mov.toFixed(2)}€ </div>
-      </div>`;
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
+      </div>
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
-const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(name => name[0])
-      .join('');
-  });
-};
-createUsernames(accounts);
-
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€ `;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
-  const outcomes = acc.movements
-    .filter(mov => mov <= 0)
+  const out = acc.movements
+    .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-
-  labelSumOut.textContent = `${Math.abs(outcomes).toFixed(2)}€ `;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -113,7 +106,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${Math.abs(interest.toFixed(2))}€ `;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 const updateUI = function (acc) {
   displayMovements(acc.movements);
